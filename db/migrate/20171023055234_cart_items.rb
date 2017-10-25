fde8035d7c78a0e61b1be2d893b001a0de27033d
meijp <mai@mais-MacBook-Pro.local>
2017-10-25 14:36:51 +0900

    fix file_scss_yaml

--- db/migrate/20171023055234_cart_items.rb
+++
@@ -1,5 +0,0 @@
-class CartItems < ActiveRecord::Migration
-  def change
-    drop_table :cart_items
-  end
-end
--- db/migrate/20171023061222_orders.rb
+++
@@ -1,5 +0,0 @@
-class Orders < ActiveRecord::Migration
-  def change
-    drop_table :orders
-  end
-end
